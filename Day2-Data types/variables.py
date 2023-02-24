0342a67c111c8449b110ae88e62aecd88bdbf8fd
samuel <samthedonz@gmail.com>
2023-02-24 15:23:58 +0100

    added variables

---
+++ Day2-Data types/variables.py
@@ -0,0 +1,5 @@
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+age = input("How old are you: ")
+
+print("Your full name is: " + first_name + " " + last_name + "and you are " + age+"years old")
