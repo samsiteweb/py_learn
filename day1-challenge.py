c4d6f18dc5db887fdec638c72ea8a4fe211e027e
samuel <samthedonz@gmail.com>
2023-02-24 14:48:09 +0100

    completed day 1 challenge

---
+++ day1-challenge.py
@@ -0,0 +1,2 @@
+print("Welcome to my Branding App")
+print("The name of the Brand should be: " + " " +input("What is the color of the bear? ") + " " + input("What is your first name? ") + "\nCongratulations")
