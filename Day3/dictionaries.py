c24829d8e6f428da326a973bb4f594b4c8a7f4b4
samuel <samthedonz@gmail.com>
2023-02-26 23:17:44 +0100

    latest

---
+++ Day3/dictionaries.py
@@ -0,0 +1,6 @@
+my_dictionary = {"c1": "value1", "c2": [1,2], "item":{"a": "boy", "b": "girl"}}
+
+print(my_dictionary)
+
+result = my_dictionary["c1"]
+print(result)
--- Day3/list.py
+++ Day3/list.py
@@ -1,3 +1,10 @@
-my_list = ['a', 'b', 'c']
-
-print(type(my_list))
+my_list = ['k', 'b', 'c']
+my_sec_list = ['e', 'd']
+print(my_list + my_sec_list)
+add_list = my_list + my_sec_list
+add_list.sort()
+add_list.reverse()
+add_list.pop(2)
+add_list.append("h")
+add_list[2] = "great"
+print(add_list)
