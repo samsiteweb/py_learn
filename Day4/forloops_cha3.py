59609000f0d5e9181f79593768999f142830894f
samuel <samthedonz@gmail.com>
2023-03-02 12:19:55 +0100

    sum using forloops

--- Day4/forloops_cha3.py
+++ Day4/forloops_cha3.py
@@ -1,5 +1,8 @@
 list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-sum_number = 0
+sum_even = 0
+sum_odd = 0
 for num in list_numbers:
-    sum_number = sum_number + num
-
+    if num % 2 == 0:
+        sum_even = sum_even + num
+    else:
+        sum_odd = sum_odd + num
