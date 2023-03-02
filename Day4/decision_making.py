e1a4d7c175bd562778ef646d41fb4c76f043d595
samuel <samthedonz@gmail.com>
2023-03-02 11:17:03 +0100

    added decision making

---
+++ Day4/decision_making.py
@@ -0,0 +1,13 @@
+
+# check if an age is within a minor range, if true print you are a minor and check if the age range passes
+
+age = int(input("Please tell me your age: "))
+print(age.ty)
+if age < 18:
+    print("you are a minor !!")
+    if age >= 7:
+        print("you should be in primary one(1)")
+    else:
+        print("You should be in primary two(2)")
+else:
+    print("You are an adult")
