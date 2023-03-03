3f8ca1f76f42f958a1d9e4113780ee2041a77672
samuel <samthedonz@gmail.com>
2023-03-03 10:07:24 +0100

    third enumerate challenge

---
+++ Day4/enumerator_cha3.py
@@ -0,0 +1,5 @@
+list_names = ["Maverick", "Alice", "Madeline", "Hazel", "Jack", "Meadow", "Thomas", "Emily", "Mills"]
+
+for ind, name in enumerate(list_names):
+     if  name.startswith('M') :
+          print(ind)
