3d408f4caea2d3291de276664cd3b2bc16190b2f
samuel <samthedonz@gmail.com>
2023-03-03 10:19:27 +0100

    zip challenge 3

---
+++ Day4/zip_cha3.py
@@ -0,0 +1,5 @@
+spanish = ["uno", "dos", "tres", "cuatro", "cinco"]
+portuguese = ["um", "dois", "três", "quatro", "cinco"]
+english = ["one", "two", "three", "four", "five"]
+
+numbers = list(zip(spanish, portuguese, english))
