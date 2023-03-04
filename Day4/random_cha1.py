5c54514d41a74f95bd21ca65b4f1bcc23ab32172
samuel <samthedonz@gmail.com>
2023-03-04 12:08:21 +0100

    random real number

--- Day4/random_cha1.py
+++ Day4/random_cha1.py
@@ -3,6 +3,10 @@ from random import choice, randint, random
 
 names = ["Samantha", "Carrie", "Chris", "Charlotte", "Richard"]
 
-
 raffle = choice(names)
+
+# generate a random real number
+real_number = random()
+
+
 
