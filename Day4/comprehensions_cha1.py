00dbfa2dd9d1ba3d9c53bd08e30b2a6c94fafdd6
samuel <samthedonz@gmail.com>
2023-03-05 23:52:39 +0100

    added comprehension challenge

--- Day4/comprehensions_cha1.py
+++ Day4/comprehensions_cha1.py
@@ -3,3 +3,9 @@ values = [1, 2, 3, 4, 5, 6, 9.5]
 square_values = [v ** 2 for v in values]
 
 print(square_values)
+
+# Create an even_values list consisting of the numbers in the values list that (you guessed it!) are even.
+
+even_values = [v for v in values if v % 2 == 0]
+
+print(even_values)
