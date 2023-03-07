95ebc2122fe6a8f8cd19b00da00a8d91d242f8b9
samuel <samthedonz@gmail.com>
2023-03-07 23:00:18 +0100

    lastest commit

--- Day3/list.py
+++ Day3/list.py
@@ -4,7 +4,11 @@ print(my_list + my_sec_list)
 add_list = my_list + my_sec_list
 add_list.sort()
 add_list.reverse()
-add_list.pop(2)
+# add_list.pop(2)
 add_list.append("h")
 add_list[2] = "great"
 print(add_list)
+
+a = my_list.pop()
+
+print(a)
