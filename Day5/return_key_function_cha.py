c1e5aee8e3a55878688bf2d9ae122d6905c699c7
samuel <samthedonz@gmail.com>
2023-03-08 01:28:07 +0100

    return key

--- Day5/return_key_function_cha.py
+++ Day5/return_key_function_cha.py
@@ -3,3 +3,16 @@
 def power(num1, num2):
     return num1 ** num2
 
+'''
+Create a function called usd_to_eur that takes a numeric value 
+(an amount in US dollars) as its only parameter, 
+and returns the equivalent amount in euros as a result.
+'''
+
+EUR = 0.90
+dollars = 1
+
+def usd_to_eur(amount_in_usd):
+    return amount_in_usd * EUR
+
+
