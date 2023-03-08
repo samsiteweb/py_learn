ab921c910966ae8a67cb50ef0dfd13103049af80
samuel <samthedonz@gmail.com>
2023-03-08 01:18:46 +0100

    function square

---
+++ Day5/functions_cha.py
@@ -0,0 +1,26 @@
+'''
+Declare a function called greet, which every time it is called prints "Hello world!"
+You should only define the function, you should not call it later.
+'''
+
+def greet():
+    print("Hello world")
+
+
+greet()
+
+
+'''
+Declare a function called welcome, which takes a person's name as an argument, and every time it is called, it prints "Welcome {name}!"
+'''
+
+
+name = "sam"
+def welcome(name):
+    print(f"Welcome {name}")
+
+
+'''
+Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
+'''
+
