272b2802d82c010540b335d7b514f10d5c62b29b
samuel <samthedonz@gmail.com>
2023-03-08 01:01:02 +0100

    learning how to use the docs

--- Day5/method_helpers_cha.py
+++ Day5/method_helpers_cha.py
@@ -1,3 +1,21 @@
 string = ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#".lstrip(',:%,_,#')
 
 print(string)
+
+# Add the element "orange" as the fourth element of the following list
+
+
+fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+
+fruits.insert(3, 'orange')
+
+print(fruits)
+
+# Check if the sets below are isolated (that is, they have no elements in common)
+
+phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+tv_brands = {"Sony", "Philips", "Samsung", "LG"}
+
+res = phone_brands.isdisjoint(tv_brands)
+
+print(res)
